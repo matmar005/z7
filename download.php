1de5a861369b1ddbeab8549d248ab10a0e05527f
matizjezior <matizjezior@wp.pl>
2016-12-08 03:54:43 +0100

    downloadV1

---
+++ download.php
@@ -0,0 +1,21 @@
+<?
+
+$file = $_POST["plik"];
+
+$filename = substr($file,0, strpos($file,"-"));
+$type = substr($file, strpos($file, "-") + 1);
+header('Pragma: public');
+header('Expires: 0');
+header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+header('Cache-Control: private', false); // required for certain browsers 
+header('Content-Type: '.$type);
+
+header('Content-Disposition: attachment; filename="'. basename($filename) . '";');
+header('Content-Transfer-Encoding: binary');
+header('Content-Length: ' . filesize($filename));
+
+readfile($filename);
+
+exit;
+
+?>
