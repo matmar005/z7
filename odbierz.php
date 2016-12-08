f0c90c8fcecb498fc5a51b252361e343e768cdde
matizjezior <matizjezior@wp.pl>
2016-12-08 02:23:47 +0100

    odbierzV2
    odbierzV2

--- odbierz.php
+++ odbierz.php
@@ -3,7 +3,7 @@ if (is_uploaded_file($_FILES['plik']['tmp_name']))
 { 
 echo 'Odebrano plik: '.$_FILES['plik']['name'].'<br/>'; 
 move_uploaded_file($_FILES['plik']['tmp_name'], 
-$_SERVER['DOCUMENT_ROOT'].$_FILES['plik']['name']); 
+$_SERVER['DOCUMENT_ROOT']."/z7/".$_FILES['plik']['name']); 
 } 
 else {echo 'Błąd przy przesyłaniu danych!';} 
 
